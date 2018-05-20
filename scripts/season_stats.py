6a2d841a0b9bdc0a0393abefabfa4cf9a0f1a032
Harrison Chase <harrison.chase@kensho.com>
2018-05-20 12:45:22 -0400

    sys path

--- scripts/season_stats.py
+++ scripts/season_stats.py
@@ -1,5 +1,5 @@
-import os
-os.chdir('/c/Users/hchase/wasserman')
+import sys
+sys.path.append('/Users/hchase/wasserman')
 
 from constants import YEAR
 from season_stats import update_season_stats_for_single_year
