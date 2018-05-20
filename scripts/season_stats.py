57a5d296c7db26271d18a12e05d2e056c9abe160
Harrison Chase <harrison.chase@kensho.com>
2018-05-20 11:09:23 -0400

    rearranging scripts

--- scripts/season_stats.py
+++ scripts/season_stats.py
@@ -7,6 +7,7 @@ import requests as _requests
 from constants import YEAR, SEASON_TYPES
 from helpers import year_to_year_string
 import nba_stats_api_constants as provider
+from sql_helpers import update_database
 
 
 for measure_type in provider.SUMMARY_STATS_TYPES:
@@ -33,32 +34,5 @@ for measure_type in provider.SUMMARY_STATS_TYPES:
     del all_df['NBA_FANTASY_PTS']
     del all_df['NBA_FANTASY_PTS1']
     columns = [col for col in all_df.columns if 'RANK' not in col]
-    all_df.to_csv('C:\\Users\\hchase\\Documents\\SQLReportCode\\CSV\\season' + type + '.csv',
-                  index=False)
-    import pyodbc
-
-    server = 'TSVMLASQL01'
-    database = 'TeamWassBasketball'
-
-    cnxn = pyodbc.connect(
-        'DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';Trusted_Connection=yes;')
-    cursor = cnxn.cursor()
-    query = '''
-	BULK INSERT season{} FROM 'C:\\Users\\hchase\\Documents\\SQLReportCode\\CSV\\season{}.csv'
-                    WITH
-                    (
-                    FIRSTROW = 2,
-                    FIELDTERMINATOR = ',',  --CSV field delimiter
-                    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-                    TABLOCK
-                    )
-	'''.format(type, type)
-    cursor.execute(query)
-    query = '''
-	DELETE 
-  	FROM season{} WHERE CONCAT(dbdate,' ',playerId,' ',season,' ', seasonType) NOT IN
-                      (
-                      SELECT CONCAT(MAX(dbdate),' ',playerId,' ',season,' ', seasonType)
-                      FROM season{} GROUP BY playerId, season, seasonType)
-	'''.format(type, type)
-    cursor.execute(query)
+    db_name = 'season{}'.format(measure_type)
+    update_database(db_name, all_df)
