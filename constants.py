7703268117825e1d6086edfe12fd20968939f23d
Harrison Chase <harrison.chase@kensho.com>
2018-05-20 16:32:13 -0400

    image fetcher

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
 YEAR = 2017
 SEASON_TYPES = ('Regular Season', 'Playoffs')
+IMAGE_TEMPLATE = ('https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/1610612759/{year}'
+                  '/260x190/{player_id}.png')
--- helpers.py
+++ helpers.py
@@ -1,4 +1,24 @@
+import os
+import urllib
+
+from constants import IMAGE_TEMPLATE
+
+
 def year_to_year_string(year):
     '''Transform a year (YYYY) to a year string (YYYY-YY)'''
     last_two_digits = str(year % 100 + 1).zfill(2)
     return '{}-{}'.format(year, last_two_digits)
+
+
+def _get_path_to_headshot(player_id):
+    '''Get path to headshot'''
+    return 'player_headshots/{}.png'.format(player_id)
+
+
+def populate_path_to_headshot(year, player_id, force_populate=False):
+    '''Return a path to a player headshot, scraping from web if desired/needed'''
+    path = _get_path_to_headshot(player_id)
+    if force_populate or not os.path.isfile(path):
+        web_url = IMAGE_TEMPLATE.format(year=year, player=player_id)
+        urllib.urlretrieve(web_url, path)
+    return path
