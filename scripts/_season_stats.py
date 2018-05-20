4173fe2308423afba9f138435657c37a58bb2413
Harrison Chase <harrison.chase@kensho.com>
2018-05-20 15:03:24 -0400

    rename and delete

--- scripts/_season_stats.py
+++ scripts/_season_stats.py
@@ -2,6 +2,6 @@ import sys
 sys.path.append('/Users/hchase/wasserman')
 
 from constants import YEAR
-from huh import update_season_stats_for_single_year
+from nba_season_stats import update_season_stats_for_single_year
 
 update_season_stats_for_single_year(YEAR)
